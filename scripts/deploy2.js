35bcc71b1bb74499789d89bcd378684f8f995244
naman <namankr91@gmail.com>
2022-01-18 13:29:20 +0530

    'svg'

--- scripts/deploy2.js
+++ scripts/deploy2.js
@@ -6,7 +6,6 @@ const main = async () => {
 
 
 
-    // console.log(Object.keys(hre.ethers))
         
     // We get the contract to deploy
     const svgNft = await hre.ethers.getContractFactory("SVGNFT");
@@ -14,18 +13,18 @@ const main = async () => {
     console.log(`You have deployed an NFT contract to ${svgDeployed.address}`)
         
     const accounts = await hre.ethers.getSigners()
-    console.log("----------------------------------------------------")
     const signer = accounts[0]
-    // const svgContract = new ethers.Contract(svgDeployed.address, svgNFTContract.interface, signer)
+    const svgContract = new hre.ethers.Contract(svgDeployed.address, svgNft.interface, signer)
+    console.log("----------------------------------------------------")
     
-    // // console.log("Let's create an NFT now!")
-    // let filepath = "./img/small_enough.svg"
-    // let svg = fs.readFileSync(filepath, { encoding: "utf8" })
-    // console.log(`We will use ${filepath} as our SVG, and this will turn into a tokenURI. `)
-    // tx = await svgDeployed.create(svg)
-    // await tx.wait(1)
-    // console.log(`You've made your first NFT!`)
-    // console.log(`You can view the tokenURI here ${await svgDeployed.tokenURI(0)}`)
+    console.log("Let's create an NFT now with the svg!")
+    const filepath = "./img/small_enough.svg"
+    const svg = fs.readFileSync(filepath, { encoding: "utf8" })
+    console.log(`We will use ${filepath} as our SVG, and this will turn into a tokenURI. `)
+    tx = await svgDeployed.create(svg)
+    await tx.wait(1)
+    console.log(`You've made your first NFT!`)
+    console.log(`You can view the tokenURI here ${await svgContract.tokenURI(0)}`)
 } catch(e) {
     console.error(e)
 }
